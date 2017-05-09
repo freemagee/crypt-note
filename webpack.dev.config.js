c2e9a037db563662a2a401371c2de3c305480943
Neil Magee <info@neilmagee.com>
2017-05-09 12:10:53 +0100

    Add postcss to dev build

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -18,7 +18,29 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader?sourceMap']
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       },
       {
         test: /\.(png|jpg|svg)$/,
