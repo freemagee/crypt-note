e462176b99f63b90bf76f4c1eef917ebf60fd494
Neil Magee <info@neilmagee.com>
2018-11-07 12:09:20 +0000

    Add build to project

---
+++ build/precache-manifest.b088dc17214a5c46911bf2600823164a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "74bce9c3501dd5774b5e",
+    "url": "/static/js/main.74bce9c3.chunk.js"
+  },
+  {
+    "revision": "934a4454f50681882ae6",
+    "url": "/static/js/1.934a4454.chunk.js"
+  },
+  {
+    "revision": "74bce9c3501dd5774b5e",
+    "url": "/static/css/main.530261a5.chunk.css"
+  },
+  {
+    "revision": "52cbb93f4e1e677e1d16974527db719f",
+    "url": "/index.html"
+  }
+];
---
+++ build/static/notes/react.md
@@ -0,0 +1,7 @@
+# REACT
+
+Step 1: Learn react
+
+Step 2: ??????
+
+Step 3: PROFIT!!!
