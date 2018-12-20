77617259df98dc3d7fa7f62402455183c92f03e1
Neil Magee <info@neilmagee.com>
2018-12-20 16:27:22 +0000

    Fix conditional rendering bug

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -6,29 +6,46 @@ export default class NotesList extends React.Component {
   setCurrentNote(id, index) {
     this.props.setCurrentNote(id, index);
   }
-  render() {
-    const notesCount = this.props.notes.length;
+  shouldComponentUpdate(nextProps) {
+    if (this.props.appMode !== nextProps.appMode) {
+      return true;
+    }
 
-    return (
-      <div className="Notes" data-app-mode={this.props.appMode}>
-        {this.props.appMode === "list" && notesCount !== 0 && (
-          <ul className="Notes__list">
-            {this.props.notes.map(function(note, index) {
-              return (
-                <NoteItem
-                  note={note}
-                  openNote={this.setCurrentNote.bind(this)}
-                  index={index}
-                  key={note.id}
-                />
-              );
-            }, this)}
-          </ul>
-        )}
-        {this.props.appMode === "list" && notesCount === 0 && (
-          <p>Currently no notes available</p>
-        )}
-      </div>
-    );
+    if (this.props.notes.length !== nextProps.notes.length) {
+      return true;
+    }
+
+    return false;
+  }
+  render() {
+    if (this.props.appMode === "list") {
+      const notesCount = this.props.notes.length;
+
+      return (
+        <div className="Notes">
+          {notesCount !== 0 && (
+            <ul className="Notes__list">
+              {this.props.notes.map(function(note, index) {
+                return (
+                  <NoteItem
+                    note={note}
+                    openNote={this.setCurrentNote.bind(this)}
+                    index={index}
+                    key={note.id}
+                  />
+                );
+              }, this)}
+            </ul>
+          )}
+          {notesCount === 0 && (
+            <p>Currently no notes available</p>
+          )}
+        </div>
+      );
+    } else {
+      return (
+        <div className="Notes isHidden"></div>
+      );
+    }
   }
 }
