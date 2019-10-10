52d624f220374ac647737bad22604c754110e934
freemagee <1529405+freemagee@users.noreply.github.com>
2019-10-10 14:56:21 +0100

    Refactor to simplify

--- src/components/RenderedNote.jsx
+++ src/components/RenderedNote.jsx
@@ -1,33 +1,20 @@
 import React from "react";
 
-import Remarkable from "remarkable";
+import { Remarkable } from "remarkable";
 import Parser from "html-react-parser";
 
 export default class RenderedNote extends React.PureComponent {
   render() {
     const md = new Remarkable();
-    const content =
-      typeof this.props.content !== "undefined"
-        ? md.render(this.props.content)
-        : "";
-    const title =
-      typeof this.props.title !== "undefined" ? this.props.title : "";
+    const mdContent = md.render(this.props.content);
 
-    if (title === "" && content === "") {
-      return (
-        <div className="RenderedNote">
-          <p>No Note Content</p>
+    return (
+      <div className="RenderedNote">
+        <div className="RenderedNote__meta">
+          <p className="RenderedNote__title">{this.props.title}</p>
         </div>
-      );
-    } else {
-      return (
-        <div className="RenderedNote">
-          <div className="RenderedNote__meta">
-            <p className="RenderedNote__title">{title}</p>
-          </div>
-          <div className="Markdown">{Parser(content)}</div>
-        </div>
-      );
-    }
+        <div className="Markdown">{Parser(mdContent)}</div>
+      </div>
+    );
   }
 }
